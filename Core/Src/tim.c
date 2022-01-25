8d9fc2489a3be49392c643108c7e820560f5fead
Daniel Bornhorst <dbornhorst@gmail.com>
2022-01-25 15:10:32 -0600

    clean up befoer adding SPI

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -44,7 +44,7 @@ void MX_TIM2_Init(void)
   htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 0xFFFF;
-  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV4;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV2;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   sConfig.EncoderMode = TIM_ENCODERMODE_TI1;
   sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
